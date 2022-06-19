ff9886b59ab119b7dc979c8981cebfbba4552617
BhakinKhantarjeerawat <64067017+BhakinKhantarjeerawat@users.noreply.github.com>
2022-06-19 20:52:38 +0700

    Update app1.py

--- app1.py
+++ app1.py
@@ -2,7 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
+import seaborn as sns
+import joblib, os
 
 
 
